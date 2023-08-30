06080afc76201b40aac052eb226848576f500daa
Vlady403 <129783181+Vlady403@users.noreply.github.com>
2023-08-30 19:09:25 +0300

    Update main.js

--- main.js
+++ main.js
@@ -11,3 +11,49 @@ navBar.addEventListener("click", () => {
     navBar.classList.toggle("changeNavBarX");
   }
 });
+
+window.addEventListener("scroll", slide);
+
+function slide() {
+  const topImages = document.querySelectorAll(".top3");
+
+  const bottomImages = document.querySelectorAll(".bottom3");
+
+  const scrHeight = window.innerHeight;
+
+  const scrWidth = screen.width;
+
+  for (let i = 0; i < topImages.length; i++) {
+    const topImgScrPos = topImages[i].getBoundingClientRect().top;
+
+    if (topImgScrPos < scrHeight) {
+      topImages[i].style.transform = "translateY(0)";
+      topImages[i].style.opacity = "1";
+    }
+  }
+
+  for (let i = 0; i < bottomImages.length; i++) {
+    const bottomImgScrPos = bottomImages[i].getBoundingClientRect().top;
+
+    if (bottomImgScrPos < scrHeight) {
+      bottomImages[i].style.transform = "translateY(0)";
+      bottomImages[i].style.opacity = "1";
+    }
+  }
+}
+
+function appear() {
+  const aboutText = document.querySelector("#aboutText");
+
+  const myImage = document.querySelector("#mainImgDiv");
+
+  const myImage2 = document.querySelector("#mainImgDiv2");
+
+  aboutText.style.transform = "translateX(0)";
+
+  myImage.style.transform = "translateX(0)";
+
+  myImage2.style.transform = "translateX(0)";
+}
+
+window.addEventListener("load", appear);
